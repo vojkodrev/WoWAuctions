186c23dfd272d91b6482202deef912bbe9476be7
Vojko Drev <vojko.drev@umwerk.com>
2018-12-03 15:01:44 +0100

    TSM class. Get realsm.

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
 const program = require('commander');
-
 const Database = require('./lib/db');
 const db = new Database();
-
+const TSM = require('./lib/tsm');
+const tsm = new TSM();
 
 program
   .version("0.0.1")
@@ -22,35 +20,14 @@ program
     await db.open();
   }
     
-
-
-  const petsCompareHtml = await rp("https://www.tradeskillmaster.com/pets/compare");
-  // console.log("petsCompareHtml", petsCompareHtml);
-
-  const petsCompare = cheerio.load(petsCompareHtml);
-
-  petsCompare("select#realmSelect option").each(async (i, e) => {
-    e = petsCompare(e);
-
-    const value = e.val();
-
-    if (value === null || value === undefined || value === "")
-      return;
-
-    const realmId = parseInt(value);
-    const name = e.text();
-
-    // Aggra (Português) (EU)
-    // Aggramar (US)
-    const nameGroups = /^(.*?) (\((.*?)\) )?\((.*?)\)$/g.exec(name);
-
+  (await tsm.getRealms()).forEach(async (e) => {
     try {
-      await db.insertRealm(realmId, nameGroups[1], nameGroups[4], nameGroups[3]);
-    } catch (e) {
-      console.log("error while inserting", realmId, name, e);
+      await db.insertRealm(e.id, e.name, e.region1, e.region2);
+    } catch (ex) {
+      console.log("error while inserting", e.id, e.name, ex);
     }
-    
-  })
+  });
+
 })();
 
 
